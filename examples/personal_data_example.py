11c5de1332f15b1f4157fe551b2c51995f8b2caa
Malcolm Hall <malcolmrainehall@gmail.com>
2021-01-16 12:38:30 -0500

    Add @cast example.

--- examples/personal_data_example.py
+++ examples/personal_data_example.py
@@ -6,6 +6,7 @@ class Person(Mutator):
         self.first_name = ""
         self.last_name = ""
         self.dob = ""
+        self.age = 0
 
     @Mutator.upper
     def get_first_name_attribute(self, value):
@@ -23,16 +24,23 @@ class Person(Mutator):
     def set_last_name_attribute(self, value):
         return value
 
-    @Mutator.datetime('%Y-%m-%dT%H:%M:%SZ')
+    @Mutator.datetime('%m/%d/%Y')
     def get_dob_attribute(self, value):
+        return value
+
+    @Mutator.cast(int)
+    def set_age_attribute(self, value):
         return value
 
 
 person = Person()
 person.first_name = "john"
 person.last_name = "doe"
-person.dob = "06/29/1983"
+person.dob = "1983-03-19T00:00:00Z"
+person.age = 55.989872
 
 print(person.first_name)
 print(person.last_name)
 print(person.dob)
+print(person.age)
+
