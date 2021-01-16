51552d64767a50533a13f84017e07236f05d3556
Malcolm Hall <malcolmrainehall@gmail.com>
2021-01-16 13:14:49 -0500

    Add non-decorator mutator example.

--- examples/personal_data_example.py
+++ examples/personal_data_example.py
@@ -7,6 +7,7 @@ class Person(Mutator):
         self.last_name = ""
         self.dob = ""
         self.age = 0
+        self.address = ""
 
     @Mutator.upper
     def get_first_name_attribute(self, value):
@@ -32,15 +33,21 @@ class Person(Mutator):
     def set_age_attribute(self, value):
         return value
 
+    def set_address_attribute(self, value):
+        # Using mutator without decorator
+        return value.upper()
+
 
 person = Person()
 person.first_name = "john"
 person.last_name = "doe"
 person.dob = "1983-03-19T00:00:00Z"
 person.age = 55.989872
+person.address = "123 abc ln. PeopleTown, USA"
 
 print(person.first_name)
 print(person.last_name)
 print(person.dob)
 print(person.age)
+print(person.address)
 
